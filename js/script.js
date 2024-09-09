82f90e7a0b9ed584b21e18c675fbea5749203e98
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-09 15:09:05 +0200

    correct receive and send a message

--- js/script.js
+++ js/script.js
@@ -182,9 +182,10 @@ createApp({
             setTimeout(() => { this.sendAnAnswer() }, 1000);
         },
         sendAMessage(content) {
-            if (content.length >= 3) {
+            if (content.length >= 2) {
                 this.contacts[this.activeIndex].messages.push({ date: '11.59', message: this.newTextSent, status: 'sent' });
                 this.newTextSent = '';
+                this.receiveAnAnswer();
             }
         },
         isNamePresent() {
